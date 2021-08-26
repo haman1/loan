323f4429843ce2bafed72543b7f3ffcb190d95b9
ombasa <ombasaherman@gmail.com>
2021-08-26 13:52:13 +0300

    Events

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,21 +2,37 @@ import React, { Component } from 'react';
 
 class Form extends Component {
     state = { }
+
+    handleChange = () => {
+        console.log('from validate form')
+
+    }
+
     render() {
         return (
             <form>
                 <div className="row">
                     <label>Amount</label>
-                    <input name="amount" class="u-full-width" type="number" placeholder="Eg:3000"></input>
+                    <input 
+
+                    onChange={this.handleChange}
+                    id="name" 
+                    name="amount" 
+                    className="u-full-width" 
+                    type="number" 
+                    placeholder="Eg:3000" />
                 </div>
                 <div>
                     <label>Months to Repay</label>
-                    <select name="months" className="u-full-height">
-                        <option value="">Select</option>
-                        <option value="3">3 Months</option>
-                        <option value="6">6 Months</option>
-                        <option value="12">12 Months</option>
-                        <option value="24">24 Months</option>
+                    <select 
+                            onChange={this.handleChange}
+                            name="months" 
+                            className="u-full-height">
+                                <option value="">Select</option>
+                                <option value="3">3 Months</option>
+                                <option value="6">6 Months</option>
+                                <option value="12">12 Months</option>
+                                <option value="24">24 Months</option>
                     </select>
                 </div>
                 <div>
